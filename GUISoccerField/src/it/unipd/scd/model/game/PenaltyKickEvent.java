793f78027e7a6f5f1a7cc584a834b92df3e6936d
Sebastiano Gottardo <dextorer@gmail.com>
2013-12-23 12:53:48 +0100

    modified game model events

--- GUISoccerField/src/it/unipd/scd/model/game/PenaltyKickEvent.java
+++ GUISoccerField/src/it/unipd/scd/model/game/PenaltyKickEvent.java
@@ -1,5 +1,8 @@
 package it.unipd.scd.model.game;
 
+import it.unipd.scd.model.Coordinate;
+import it.unipd.scd.model.Team;
+
 /**
  * Created with IntelliJ IDEA.
  * User: dextor
@@ -8,6 +11,19 @@ package it.unipd.scd.model.game;
  * To change this template use File | Settings | File Templates.
  */
 public class PenaltyKickEvent extends GameEvent {
+
+    private int id;
+    private int number;
+
+    private Team team;
+    private Coordinate coordinate;
+
+    public PenaltyKickEvent(int id, int number, Team team, Coordinate coord) {
+        this.id = id;
+        this.number = number;
+        this.team = team;
+        this.coordinate = coord;
+    }
 
     @Override
     public void draw() {
